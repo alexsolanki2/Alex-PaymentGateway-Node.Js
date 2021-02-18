b4fd5425e0d149f50a8e44bed1f653990a13f27d
alexsolanki2 <46710296+alexsolanki2@users.noreply.github.com>
2021-02-18 23:49:51 +0530

    change in callback url

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['ORDER_ID'] = 'TEST_'  + new Date().getTime();
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
-    params['CALLBACK_URL'] = `http://localhost:${PORT}/callback` || 'http://localhost:4000/callback';
+    params['CALLBACK_URL'] = '/callback' || 'http://localhost:4000/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
 
