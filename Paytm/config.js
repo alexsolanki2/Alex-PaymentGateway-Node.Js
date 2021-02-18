34eb54593df4151276b31465a50410b359fb0394
alexsolanki2 <46710296+alexsolanki2@users.noreply.github.com>
2021-02-19 03:11:23 +0530

    =Email Integration 2

--- Paytm/config.js
+++ Paytm/config.js
@@ -2,12 +2,9 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 var PaytmConfig = {
-  mid: process.env.PAYTM_MID,
-  key: process.env.PAYTM_KEY,
+  mid: "iUqkAU13892783038523",
+  key: "Ktty_yVv6!hU7TU7",
   website: "WEBSTAGING",
 };
-
-console.log(process.env.PAYTM_MID)
-console.log(process.env.PAYTM_KEY)
 
 module.exports.PaytmConfig = PaytmConfig;
