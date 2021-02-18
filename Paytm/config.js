ec9274fec993a5836c01fc0154e5ab6f6ef4e76d
alexsolanki2 <46710296+alexsolanki2@users.noreply.github.com>
2021-02-19 03:37:45 +0530

    =Email Integration 3 (Final)

--- Paytm/config.js
+++ Paytm/config.js
@@ -2,8 +2,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 var PaytmConfig = {
-  mid: "iUqkAU13892783038523",
-  key: "Ktty_yVv6!hU7TU7",
+  mid: process.env.PAYTM_MID,
+  key: process.env.PAYTM_KEY,
   website: "WEBSTAGING",
 };
 
--- emails/accounts.js
+++ emails/accounts.js
@@ -6,11 +6,11 @@ const sendGridAPIKey = process.env.SENDGRID_API_KEY
 
 sgMail.setApiKey(sendGridAPIKey)
 
-const sendTransSuccess = (order, amt, curr, mode, date, bank) => sgMail.send({
+const sendTransSuccess = (order, amt, mode, date, bank) => sgMail.send({
     to: 'alexss.solanki1@gmail.com',
     from: 'alexss.solanki1@gmail.com',
     subject: 'Invoice: Transaction Successful XD (Alex-Payment-Gateway)',
-    text: `   Your Order ID: ${order}
+    text: `    Your Order ID: ${order}
      Amount You Paid: ${amt}
      Mode Of Payment: ${mode}
      Date And Time Of Transaction ${date}
@@ -20,18 +20,16 @@ const sendTransSuccess = (order, amt, curr, mode, date, bank) => sgMail.send({
      Thank You`
 })
 
-const sendTransFail = (order, amt, curr, mode, date, bank) => sgMail.send({
+const sendTransFail = (order, amt, mode, date, bank) => sgMail.send({
     to: 'alexss.solanki1@gmail.com',
     from: 'alexss.solanki1@gmail.com',
     subject: 'Invoice: !! Transaction Failed !! (Alex-Payment-Gateway)',
-    text: `   Your Order ID: ${order}
+    text: `    Your Order ID: ${order}
      Amount You Paid: ${amt}
      Mode Of Payment: ${mode}
      Date And Time Of Transaction ${date}
      Bank Name: ${bank}`
 })
-
-console.log(sendGridAPIKey)
 
 module.exports = {
     sendTransSuccess,
